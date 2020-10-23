2481ef1c82dfd455fdac96e72fadad3a34220568
Kalpesh Shah <kalpeshshah1008@gmail.com>
2020-10-24 00:38:06 +0530

    Create workerbeta.js
    Hey, I have just added Xylophone MIDI instead of the old ones.

---
+++ assets/js/workerbeta.js
@@ -0,0 +1,42 @@
+var AudioContext = window.AudioContext || window.webkitAudioContext || false; 
+var ac	= new AudioContext || new webkitAudioContext;
+var digits = new Array();
+function playFunction(){
+    document.getElementById("playButton").innerText = "Playing...";
+    document.getElementById("playButton").disabled  = true;
+    
+    //generating first 30 fibonacci numbers
+    for(let i = 0 ;i<30;i++){
+      digits = digits.concat(fibonacci(i).toString().split(''));
+    }
+    playSomthing();  
+}
+function playSomthing(){
+Soundfont.instrument(ac, 
+                    'https://raw.githubusercontent.com/gleitz/midi-js-soundfonts/gh-pages/FluidR3_GM/xylophone-mp3/B2.mp3.js') //replaced withxylophone MIDI
+                    .then(function (instrument) {
+                        for(let j=0; j<digits.length;j++){
+                            if(parseInt(digits[j]) < 8 && parseInt(digits[j]) > 0){
+                                instrument.schedule(this.ac.currentTime, [{note: 'D'+digits[j], time:j/5}]) 
+                             } 
+                              else {
+                                if (parseInt(digits[j])<=0) {
+                                    let value = parseInt(digits[j])+4;
+                                    instrument.schedule(this.ac.currentTime, [{note: 'A'+value, time:j/5}]) 
+                                }else
+                                {
+                                    let value = parseInt(digits[j])-2;
+                                     instrument.schedule(this.ac.currentTime, [{note: 'E'+value, time:j/5}]) 
+
+                                }
+                            }
+                        }
+                    }).catch(function(err){ 
+                        console.log(err)
+                     })
+}
+function fibonacci(num) {
+    if(num == 0) return 0;
+    if (num == 1) return 1;
+    return fibonacci(num - 1) + fibonacci(num - 2);
+}
